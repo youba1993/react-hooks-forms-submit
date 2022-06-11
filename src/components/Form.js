3b0dc12f460cb72debb04c37d54afc58dd917426
youbaidjis <youba.etud@gmail.com>
2022-06-10 21:03:30 -0700

    Completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,54 +1,62 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [formData, setFormData] = useState({
-    firstName: "John",
-    lastName: "Henry",
-    admin: false,
+  const [firstName, setFirstName] = useState("John");
+  const [lastName, setLastName] = useState("Henry");
+  const [submittedData, setSubmittedData] = useState([]);
+
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
+
+  const [errors, setErrors] = useState([]);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  // first name is required
+  if (firstName.length > 0) {
+    const formData = { firstName: firstName, lastName: lastName };
+    const dataArray = [...submittedData, formData];
+    setSubmittedData(dataArray);
+    setFirstName("");
+    setLastName("");
+    setErrors([]);
+  } else {
+    setErrors(["First name is required!"]);
+  }
+}
+
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    );
   });
 
-  function handleChange(event) {
-    const name = event.target.name;
-    let value = event.target.value;
-
-    // use `checked` property of checkboxes instead of `value`
-    if (event.target.type === "checkbox") {
-      value = event.target.checked;
-    }
-
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log(formData);
-  }
-
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="firstName"
-        onChange={handleChange}
-        value={formData.firstName}
-      />
-      <input
-        type="text"
-        name="lastName"
-        onChange={handleChange}
-        value={formData.lastName}
-      />
-      <input
-        type="checkbox"
-        name="admin"
-        onChange={handleChange}
-        checked={formData.admin}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      {/* conditionally render error messages */}
+      {errors.length > 0
+        ? errors.map((error, index) => (
+            <p key={index} style={{ color: "red" }}>
+              {error}
+            </p>
+          ))
+        : null}
+      <h3>Submissions</h3>
+      {listOfSubmissions}
+    </div>
   );
 }
+
 export default Form;
