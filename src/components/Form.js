c865e6f8c07f8c9957f5625d1d316e96a3eef71a
youbaidjis <youba.etud@gmail.com>
2022-06-10 20:56:54 -0700

    Completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,59 +1,54 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
-  const [submittedData, setSubmittedData] = useState([]);
-  const [errors, setErrors] = useState([]);
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry",
+    admin: false,
+  });
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
+  function handleChange(event) {
+    const name = event.target.name;
+    let value = event.target.value;
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
+    // use `checked` property of checkboxes instead of `value`
+    if (event.target.type === "checkbox") {
+      value = event.target.checked;
+    }
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    if (firstName.length > 0) {
-      const formData = { firstName: firstName, lastName: lastName };
-      const dataArray = [...submittedData, formData];
-      setSubmittedData(dataArray);
-      setFirstName("");
-      setLastName("");
-      setErrors([]);
-    } else {
-      setErrors(["First name is required!"]);
-    }
+    console.log(formData);
   }
 
-  const listOfSubmissions = submittedData.map((data, index) => {
-    return (
-      <div key={index}>
-        {data.firstName} {data.lastName}
-      </div>
-    );
-  });
-
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleFirstNameChange} value={firstName} />
-        <input type="text" onChange={handleLastNameChange} value={lastName} />
-        <button type="submit">Submit</button>
-      </form>
-      {errors.length > 0
-      ? errors.map((error, index) => (
-          <p key={index} style={{ color: "red" }}>
-            {error}
-          </p>
-        ))
-      : null}
-      <h3>Submissions</h3>
-      {listOfSubmissions}
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="firstName"
+        onChange={handleChange}
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        name="lastName"
+        onChange={handleChange}
+        value={formData.lastName}
+      />
+      <input
+        type="checkbox"
+        name="admin"
+        onChange={handleChange}
+        checked={formData.admin}
+      />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
-
 export default Form;
